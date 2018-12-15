58e23a7b90b5ada1e7fc0f464f93ac31325eec51
Roxana.Panait <ghioroaie.roxana@yahoo.com>
2018-12-16 00:25:33 +0200

    Am adaugat un mesaj de inceput si un System.exit

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Afla patratul unui numar");
         Scanner sc = new Scanner(System.in);
         System.out.println("Introdu un numar: ");
         int x=sc.nextInt();
         System.out.println("Rezultatul este: " + x*x);
+        System.exit(0);
     }
 }
