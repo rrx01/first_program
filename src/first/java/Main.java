69cffec5eafc3748b8336547392f33faaa4bba2f
Roxana.Panait <ghioroaie.roxana@yahoo.com>
2018-12-16 00:36:20 +0200

    Am formatat codul.

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -8,8 +8,8 @@ public class Main {
         System.out.println("Afla patratul unui numar");
         Scanner sc = new Scanner(System.in);
         System.out.println("Introdu un numar: ");
-        int x=sc.nextInt();
-        System.out.println("Patratul numarului " + x + " este " + (int) Math.pow(x,2));
+        int x = sc.nextInt();
+        System.out.println("Patratul numarului " + x + " este " + (int) Math.pow(x, 2) + ".");
         System.exit(0);
     }
 }
